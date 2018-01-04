223e0ae277dffd43a32f7eae30afb10c949b7955
aapujji <aapujji@gmail.com>
2018-01-04 12:49:50 -0500

    Update newsdata.py

--- newsdata.py
+++ newsdata.py
@@ -7,15 +7,15 @@ query_one = "What are the most popular three articles of all time?\n"
 query_two = "Who are the most popular article authors of all time?\n"
 query_three = "On which days did more than 1% of requests lead to errors?\n"
 
-query_one_ans = ("select title, count(*) from articles join log "
-                 "on log.path like concat('%',articles.slug,'%') "
-                 "group by title, path order by count(*) desc limit 3;")
-query_two_ans = ("select name, count(*) from authors join articles on "
-                 "articles.author = authors.id join log on log.path "
-                 "like concat('%', articles.slug, '%') group by name,"
-                 "log.path order by count(*) desc limit 3;")
-query_three_ans = ("select date, perc from err_perc group by date,"
-                   "perc having perc >= 1 order by perc;")
+query_one_ans = ("SELECT title, count(*) FROM articles JOIN log "
+                 "ON log.path LIKE concat('%',articles.slug,'%') "
+                 "GROUP BY title, path ORDER BY count(*) DESC limit 3;")
+query_two_ans = ("SELECT name, count(*) FROM authors JOIN articles ON "
+                 "articles.author = authors.id JOIN log ON log.path "
+                 "LIKE concat('%', articles.slug, '%') GROUP BY name,"
+                 "log.path ORDER BY count(*) DESC limit 3;")
+query_three_ans = ("SELECT date, perc FROM err_perc GROUP BY date,"
+                   "perc HAVING perc >= 1 ORDER BY perc;")
 
 
 def get_query(query):
